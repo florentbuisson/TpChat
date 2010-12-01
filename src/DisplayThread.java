3ce731ea8df83a71ffbed1b9ed64400cd8f84ed5
Elsa <renard.mail@gmail.com>
2010-12-01 11:37:35 +0100

    elsa3

---
+++ src/DisplayThread.java
@@ -0,0 +1,25 @@
+
+
+public class DisplayThread extends Thread {
+	
+	ChatInterface serv;
+	ChatClient cli;
+	
+	public DisplayThread(String s, ChatInterface serv, ChatClient cli) {
+		super(s);
+		this.serv = serv;
+		this.cli = cli;
+	}
+	
+	public void run() {
+		while (true) {
+			try {
+				serv.displayMsgs(cli.getIdDernier(), cli.getNom());
+				sleep(30000);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ src/UserThread.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class UserThread extends DisplayThread {
+	
+	public UserThread(String s, ChatInterface serv, ChatClient cli) {
+		super(s, serv, cli);
+	}
+	
+	public void run() {
+		while (true) {
+			Scanner sc = new Scanner(System.in);
+			String line = sc.nextLine();
+		}
+	}
+	
+	public void appliquer(int cas, String line) {
+		switch (cas) {
+		case 1:
+			line = line.substring(5);
+			serv.send
+		}
+	}
+	
+	public int trierLigne(String line) {
+		int resTri = 0;
+		
+		if (line.substring(0, 5).equals("send ")) {
+			resTri = 1;
+		} else if (line.substring(0, 4).equals("bye ")) {
+			resTri = 2;
+		}
+		
+		return resTri;
+	}
+
+}
