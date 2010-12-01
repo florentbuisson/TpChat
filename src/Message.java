89b22d4fe2935b683cbae5e7f614ab05aa4f6cfc
florentbuisson <florent.buisson@eleves.ec-nantes.fr>
2010-12-01 10:40:15 +0100

    MessageAjoute

---
+++ src/Message.java
@@ -0,0 +1,19 @@
+
+public class Message {
+
+	private int id;
+	private String contenu;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContenu() {
+		return contenu;
+	}
+	public void setContenu(String contenu) {
+		this.contenu = contenu;
+	}
+}
