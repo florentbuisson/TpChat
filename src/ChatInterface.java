0fba99dce89966fcd29d0aa6e8fad6e0a7af3254
Elsa <renard.mail@gmail.com>
2010-12-01 10:20:53 +0100

    elsa2

--- src/ChatInterface.java
+++ src/ChatInterface.java
@@ -2,6 +2,12 @@ import java.rmi.RemoteException;
 
 public interface ChatInterface {
 	
-	public void connecter() throws RemoteException;
+	public void connect(String nom) throws RemoteException;
+	
+	public void bye(String nom) throws RemoteException;
+	
+	public void who() throws RemoteException;
+	
+	public void displayMsgs(int idDernier, String nom) throws RemoteException;
 
 }
